8bc30afdf95b5c825c034b0cc93195a7572e4759
abduallah <abduallahkadry2001@gmail.com>
2025-09-01 23:33:12 +0300

    FIrst COmmit

---
+++ src/test/java/com/app/coursecenter/CourseCenterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.app.coursecenter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseCenterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
