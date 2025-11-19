dd1189d2e13d097ee549ca375f6bf76db0481608
spectaks <you@example.com>
2025-11-19 12:30:03 +0200

    - rating a course and getting the average rating - Merged with readme

--- src/main/java/com/app/coursecenter/controller/frontend/CoursePageViewController.java
+++ src/main/java/com/app/coursecenter/controller/frontend/CoursePageViewController.java
@@ -2,23 +2,23 @@ package com.app.coursecenter.controller.frontend;
 
 
 import com.app.coursecenter.entity.Course;
-import com.app.coursecenter.request.CreateCourseRequest;
 import com.app.coursecenter.response.CourseResponse;
 import com.app.coursecenter.service.courseservice.CourseService;
-import org.springframework.beans.factory.annotation.Autowired;
+import com.app.coursecenter.service.userservice.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequiredArgsConstructor
 @RequestMapping("${app.paths.frontend-base}${app.paths.course-base}") // page
 public class CoursePageViewController {
-    @Autowired
-    CourseService courseService;
+
+    private final CourseService courseService;
+    private final UserService userService;
+
 
     @GetMapping("")
     public String courses(
@@ -53,15 +53,11 @@ public class CoursePageViewController {
 
     @GetMapping("/{id}")
     public String showCourseDetail(@PathVariable Long id, Model model) {
-        System.out.println("iam here");
-        System.out.println();
-        System.out.println("iam here");
 
         model.addAttribute("isEnrolled", userService.getEnrolledCourses().stream().anyMatch(s -> s.getId().equals(id)));
 
         model.addAttribute("course", courseService.getCourseById(id));
         return "course-details";
     }
-
 
 }
